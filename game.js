6d23209188f927e243eb3de2160e1798f7d0fad1
AdithiJhanavi <141707677+AdithiJhanavi@users.noreply.github.com>
2023-11-21 21:41:33 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
-// Iteration 1: Declare variables required for this game
+//Declaring and assigning values for the variables and constants
 const gameBody = document.getElementById("game-body");
 const $lives = document.getElementById("lives");
-var sec = document.getElementById("timer").textContent;
-var id = 0;
+var seconds = document.getElementById("timer").textContent;
+var zombieId = 0;
 const img = [
   "zombie-1.png",
   "zombie-2.png",
@@ -10,29 +10,32 @@ const img = [
   "zombie-5.png",
   "zombie-6.png",
 ];
-// Iteration 1.2: Add shotgun sound
-const shotgunAudio = new Audio("./assets/shotgun.wav");
-shotgunAudio.volume = 0.5;
+// Gun shot sound
+const expAudio = new Audio(
+  "https://freespecialeffects.co.uk/soundfx/weapons/shotgun_3.wav"
+);
+expAudio.volume = 0.2;
 gameBody.onclick = () => {
-  shotgunAudio.pause();
-  shotgunAudio.currentTime = 0;
-  shotgunAudio.play();
+  expAudio.pause();
+  expAudio.currentTime = 0;
+  expAudio.play();
 };
-// Iteration 1.3: Add background sound
-const backgroundSound = new Audio("./assets/bgm.mp3");
+// Background sound
+const backgroundSound = new Audio(
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/250758/soundtrack.mp3"
+);
 backgroundSound.play();
 backgroundSound.loop = true;
 
-// Iteration 1.4: Add lives
-const maxlives = 4;
+//Lives
 var lives = 4;
 
-// Iteration 2: Write a function to make a zombie
+//Iteration 5: Write a function to create a zombie
 
 function makeZombie() {
   randomImage = img[getRandomInt(0, img.length)];
-  gameBody.innerHTML += `<img src="./assets/${randomImage}" class="zombie-image" id="zombie${id}">`;
-  let zombie = document.getElementById("zombie" + id);
+  gameBody.innerHTML += `<img src="https://cdn.jsdelivr.net/gh/Kalvium-Program/zombie-shoot-game-final@main/assets/${randomImage}" class="zombie-image" id="zombie${zombieId}">`;
+  let zombie = document.getElementById("zombie" + zombieId);
   zombie.style.transform = `translateX(${getRandomInt(20, 80)}vw)`;
   zombie.style.animationDuration = `${getRandomInt(2, 6)}s`;
   zombie.onclick = () => {
@@ -40,7 +43,7 @@ function makeZombie() {
   };
 }
 
-// Iteration 3: Write a function to check if the player missed a zombie
+// Iteration 6: Write a Function to find if the player missed a zombie
 
 function checkCollision(zombie) {
   if (zombie.getBoundingClientRect().top <= 0) {
@@ -50,20 +53,20 @@ function checkCollision(zombie) {
   return false;
 }
 
-// Iteration 4: Write a function to destroy a zombie when it is shot or missed
+// Iteration 7: Write a Function to destroy a zombie when it is shot
 
 function zombieDestruct(zombie) {
   zombie.style.display = "none";
-  id++;
+  zombieId++;
   makeZombie();
 }
 
-// Iteration 5: Creating timer
+// Timer
 
 var timer = setInterval(function () {
-  sec--;
-  document.getElementById("timer").textContent = sec;
-  let zombie = document.getElementById("zombie" + id);
+  seconds--;
+  document.getElementById("timer").textContent = seconds;
+  let zombie = document.getElementById("zombie" + zombieId);
   if (checkCollision(zombie) == true) {
     zombieDestruct(zombie);
     if (lives == 0) {
@@ -71,20 +74,18 @@ var timer = setInterval(function () {
       location.href = "./game-over.html";
     }
   }
-  if (sec == 0) {
+  if (seconds == 0) {
     clearInterval(timer);
     location.href = "./win.html";
   }
 }, 1000);
 
-// Iteration 6: Write a code to start the game by calling the first zombie
+// Start the game by calling the first zombie
 
-makeZombie(id);
+makeZombie();
 
-// Iteration 7: Write the helper function to get random integer
+// Helper function to get random integer
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
